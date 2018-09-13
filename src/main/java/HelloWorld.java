cb2eecfaca74b51b3052145e9cfa70a264dca28a
yiranyizha <649528021@qq.com>
2018-09-13 17:37:18 +0800

    first
    Signed-off-by: yiranyizha <649528021@qq.com>

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hellokk");
         System.out.println("hehe");
+        System.out.println("heiheihhie");
     }
 }
